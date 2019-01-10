dc3d8bda1fb92974a0696b996e1813af03680a55
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2019-01-09 22:33:54 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError: # for pip <= 9.0.3
 
 from setuptools import setup, find_packages
 
-version = '0.3.4'
+version = '0.3.5'
 
 long_description = """
 freezing-model is the database model and message definitions shared by freezing saddles components.
