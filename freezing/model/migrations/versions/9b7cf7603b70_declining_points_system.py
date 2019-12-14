e9ee9fa9e1a554464e636859cd5c280cc95e27a8
Richard Bullington-McGuire <richard@bullington-mcguire.com>
2019-12-14 12:04:00 -0500

    Fix downrevision & syntax & line length
    PEP-8 is a harsh master

--- freezing/model/migrations/versions/9b7cf7603b70_declining_points_system.py
+++ freezing/model/migrations/versions/9b7cf7603b70_declining_points_system.py
@@ -5,14 +5,15 @@ from freezing.model.config import config as model_config
 """Update daily_scores view to refelect declining points for first 10 miles
 
 Revision ID: 9b7cf7603b70
-Revises: c8add074d30f
+Revises: 12a5e1aff276
 Create Date: 2019-12-14 11:38:19.648650
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '9b7cf7603b70'
-down_revision = 'c8add074d30f'
+down_revision = '12a5e1aff276'
+
 
 def upgrade():
     op.execute("""
@@ -20,7 +21,8 @@ def upgrade():
         select A.team_id, R.athlete_id, sum(R.distance) as distance,
         case
             when distance < 0 then 0
-            when distance < 10 then 10 + 0.5 * (21 * distance - (distance * distance))
+            when distance < 10
+                then 10 + 0.5 * (21 * distance - (distance * distance))
             else 65 + distance
         end as points,
         date(CONVERT_TZ(R.start_date, R.timezone,'{0}')) as ride_date
@@ -31,7 +33,7 @@ def upgrade():
           A.team_id,
           date(CONVERT_TZ(R.start_date, R.timezone,'{0}'))
         ;
-        """).format(model_config.TIMEZONE))
+        """.format(model_config.TIMEZONE))
 
 
 def downgrade():
