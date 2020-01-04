2116c884705c5ff81920420853bd0091abbfb862
Richard Bullington-McGuire <richard@bullington-mcguire.com>
2020-01-03 23:38:34 -0500

    Fix view - no subselects in from clause allowed

--- freezing/model/migrations/versions/f620a24f5f7e_least_variance.py
+++ freezing/model/migrations/versions/f620a24f5f7e_least_variance.py
@@ -16,25 +16,28 @@ down_revision = 'b4d003c71167'
 
 def upgrade():
     op.execute("""
-               create or replace view variance_by_day as
-               select
-                 ds.athlete_id,
-                 sum(case when ds.points > 10 then 1 else 0 end) ride_days,
-                 sum(distance) total_miles,
-                 var_pop(case when ds.dow=1 then ds.distance end) sun_var_pop,
-                 var_pop(case when ds.dow=2 then ds.distance end) mon_var_pop,
-                 var_pop(case when ds.dow=3 then ds.distance end) tue_var_pop,
-                 var_pop(case when ds.dow=4 then ds.distance end) wed_var_pop,
-                 var_pop(case when ds.dow=5 then ds.distance end) thu_var_pop,
-                 var_pop(case when ds.dow=6 then ds.distance end) fri_var_pop,
-                 var_pop(case when ds.dow=7 then ds.distance end) sat_var_pop
-               from
-                 (select
-                    *,
-                    dayofweek(ride_date) as dow
-                  from daily_scores) ds
-               group by ds.athlete_id;
-               ;
+             create or replace view variance_by_day as
+                select
+                  ds.athlete_id,
+                  sum(case when ds.distance >= 1 then 1 else 0 end) ride_days,
+                  sum(distance) total_miles,
+                  var_pop(case when dayofweek(ds.ride_date)=1
+                               then ds.distance end) sun_var_pop,
+                  var_pop(case when dayofweek(ds.ride_date)=2
+                               then ds.distance end) mon_var_pop,
+                  var_pop(case when dayofweek(ds.ride_date)=3
+                               then ds.distance end) tue_var_pop,
+                  var_pop(case when dayofweek(ds.ride_date)=4
+                               then ds.distance end) wed_var_pop,
+                  var_pop(case when dayofweek(ds.ride_date)=5
+                               then ds.distance end) thu_var_pop,
+                  var_pop(case when dayofweek(ds.ride_date)=6
+                               then ds.distance end) fri_var_pop,
+                  var_pop(case when dayofweek(ds.ride_date)=7
+                               then ds.distance end) sat_var_pop
+                from
+                  daily_scores ds
+                group by ds.athlete_id;
                """)
 
 
