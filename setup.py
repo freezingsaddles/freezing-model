75d14b14525d97801da151977696b3a258ea8aff
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2020-11-19 13:53:36 -0500

    Simplify

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ import os.path
 import re
 import warnings
 
-# Ugh, pip 10 is backward incompatible, but there is a workaround:
-# Thanks Stack Overflow https://stackoverflow.com/a/49867265
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+# Thanks https://stackoverflow.com/a/39041067
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 from setuptools import setup, find_packages
 
@@ -18,17 +17,7 @@ long_description = """
 freezing-model is the database model and message definitions shared by freezing saddles components.
 """
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements(os.path.join(os.path.dirname(__file__), 'requirements.txt'), session=False)
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-# This started failing on some Docker builds, for example: https://hub.docker.com/repository/registry-1.docker.io/freezingsaddles/freezing-web/builds/3b84dc71-e91b-4e7e-af3f-3eb41b40d5b8
-# Thanks Mehant Kammakomati and Stack Overflow for the fix: https://stackoverflow.com/a/62127548
-try:
-    reqs = [str(ir.req) for ir in install_reqs]
-except:
-    reqs = [str(ir.requirement) for ir in install_reqs]
 
 setup(
     name='freezing-model',
@@ -42,7 +31,7 @@ setup(
     packages=['freezing.model', 'freezing.model.msg'],
     include_package_data=True,
     package_data={'freezing.model': ['migrations/*']},
-    install_requires=reqs,
+    install_requires=install_reqs,
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
