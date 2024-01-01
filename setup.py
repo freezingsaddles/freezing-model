20d49bef9c981f9c55af3a475a6ea0aa0a0701b0
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2023-12-31 22:27:15 -0500

    Bump version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import warnings
 
 from setuptools import setup, find_packages
 
-version = '0.6.0'
+version = '0.7.0'
 
 long_description = """
 freezing-model is the database model and message definitions shared by freezing saddles components.
