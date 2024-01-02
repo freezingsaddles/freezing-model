0bdb91d1c606071b6c148ac32b34522790959703
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2024-01-01 20:29:13 -0500

    Bump version for release, reformat

--- setup.py
+++ setup.py
@@ -1,43 +1,40 @@
 # -*- coding: utf-8 -*-
-import os.path
-import re
-import warnings
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-version = '0.7.6'
+version = "0.7.7"
 
 long_description = """
-freezing-model is the database model and message definitions shared by freezing saddles components.
+freezing-model is the database model and message definitions
+shared by Freezing Saddles components.
 """
 
 install_reqs = [
-'SQLAlchemy',
-'GeoAlchemy',
-'alembic',
-'marshmallow',
-'marshmallow-enum',
-'PyMySQL'
+    "SQLAlchemy",
+    "GeoAlchemy",
+    "alembic",
+    "marshmallow",
+    "marshmallow-enum",
+    "PyMySQL",
 ]
 
 setup(
-    name='freezing-model',
+    name="freezing-model",
     version=version,
-    author='Hans Lellelid',
-    author_email='hans@xmpl.org',
-    url='http://github.com/freezingsaddles/freezing-model',
-    license='Apache',
-    description='Freezing Saddles database and messaging models.',
+    author="Hans Lellelid",
+    author_email="hans@xmpl.org",
+    url="http://github.com/freezingsaddles/freezing-model",
+    license="Apache",
+    description="Freezing Saddles database and messaging models.",
     long_description=long_description,
-    packages=['freezing.model', 'freezing.model.msg'],
+    packages=["freezing.model", "freezing.model.msg"],
     include_package_data=True,
-    package_data={'freezing.model': ['migrations/*']},
+    package_data={"freezing.model": ["migrations/*"]},
     install_requires=install_reqs,
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
     ],
-    zip_safe=True
+    zip_safe=True,
 )
