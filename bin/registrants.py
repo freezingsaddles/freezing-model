ed26ff10dac6c9b3cd4d665dad9d1e580e0ef045
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2024-12-07 14:18:10 -0500

    Out with Powershell, in with Python
    Thanks GitHub Copilot for the boost
    
    Protect against SQL injection also

--- bin/registrants.py
+++ bin/registrants.py
@@ -10,24 +10,36 @@ import csv
 import sys
 from datetime import datetime
 
+from pymysql.converters import escape_string
+
+
 def main(csvfile):
     print("drop table if exists registrants;")
-    print("create table registrants (regnum int(11), id int(11), username varchar(255), name varchar(255), email varchar(255), registered_on datetime);")
+    print(
+        "create table registrants (regnum int(11), id int(11), username varchar(255), name varchar(255), email varchar(255), registered_on datetime);"
+    )
     print("begin;")
 
-    with open(csvfile, newline='') as csvfile:
+    with open(csvfile, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            regnum = row["#"]
-            id = row["Strava user ID"]
-            firstname = row["First name"]
-            lastname = row["Last name"]
-            username = row["Your user name on the Washington Area Bike Forum"]
-            email = row["E-mail"]
-            datesubmitted = datetime.strptime(row["Date Submitted"], '%B %d, %Y %I:%M %p')
-            datesubmitted_str = datesubmitted.strftime('%Y-%m-%d %H:%M:%S')
-            print(f"insert into registrants values({regnum}, {id}, '{username}', '{firstname} {lastname}', '{email}', '{datesubmitted_str}');")
+            regnum = int(row["#"])
+            id = int(row["Strava user ID"])
+            firstname = escape_string(row["First Name"])
+            lastname = escape_string(row["Last Name"])
+            username = escape_string(
+                row["Your user name on the Washington Area Bike Forum"]
+            )
+            email = escape_string(row["E-mail"])
+            datesubmitted = datetime.strptime(
+                row["Date Submitted"], "%B %d, %Y %I:%M %p"
+            )
+            datesubmitted_str = datesubmitted.strftime("%Y-%m-%d %H:%M:%S")
+            print(
+                f"insert into registrants values({regnum}, {id}, '{username}', '{firstname} {lastname}', '{email}', '{datesubmitted_str}');"
+            )
     print("commit;")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
